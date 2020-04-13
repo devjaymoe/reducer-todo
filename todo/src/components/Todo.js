e9dc26f75f687d981ca096e80d77292dc347d721
Devin_Moore <d.j.moore209@gmail.com>
2020-04-13 16:23:11 -0400

    rendering list of todos

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -2,18 +2,32 @@ import React, { useState, useReducer} from 'react';
 import { reducer, initialState } from '../reducers/reducer';
 
 function Todo (){
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [todo, dispatch] = useReducer(reducer, initialState);
 
-    const [todo, setItem] = useState({
-        task: 'Complete todo list',
-        completed: false,
-        id: Date.now()
-    });
+    // const [todo, setItem] = useState({
+    //     task: 'Complete todo list',
+    //     completed: false,
+    //     id: Date.now()
+    // });
 
     const handleChanges = e => {
-        setItem({ [e.target.name]: e.target.value});
-      };
+        dispatch({ [e.target.name]: e.target.value});
+    };
 
+    // toggleTask = taskId => {
+    // //console.log(taskId)
+    //     setState({
+    //         list: this.state.list.map(item => {
+    //         if( taskId === item.id ){
+    //             return {
+    //             ...item,
+    //             completed: !item.completed
+    //             };
+    //         }
+    //         return item;
+    //         })
+    //     })
+    // }
     return (
         <div>
             <div>
@@ -32,6 +46,18 @@ function Todo (){
                 Add Todo
                 </button>
             </div>
+
+            <ul className='task-container'>
+                {todo.map( item => (
+                    <div
+                        className={`task ${item.completed ? 'completed' : ''}`}
+                        // onClick={() => state.toggleTask(props.item.id)}
+                    >
+                        <li>{item.task}</li>
+                    </div>
+                ))}
+            </ul>
+
         </div>
     );
 }
--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export const initialState = {
-    task: 'Complete todo list',
-    completed: false,
-    id: Date.now()
-}
+export const initialState = [
+    {
+        task: 'Complete todo list',
+        completed: false,
+        id: Date.now()
+    }
+]
 
 export const reducer = (state, action) => {
     switch (action.type) {
